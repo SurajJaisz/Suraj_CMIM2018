44ce6bf66d6b174a593a8899b0db67e5db0f67d2
h6804 <h6804@LUT7283.win.lut.fi>
2018-05-14 02:14:06 +0300

    Defining Bodies and Joints

--- Homework-7 (Kinematic Analysis)/INPUT_Data.m
+++ Homework-7 (Kinematic Analysis)/INPUT_Data.m
@@ -1,41 +1,64 @@
 clear; clc; close all;
+InputParameters = struct(); % Structure to input all the parameters
 
-%% INPUT THE BODIES DETAILS HERE
+%% Defining Time Step and Run Time for Simulation
+tstep = 0.01; % in seconds
+tfinal = 2; % in seconds
+InputParameters.tspan = 0:tstep:tfinal;
 
+%% Defining Numerical Parameters for the System
+L = 1; % in meters
+omega = -1;
+
+%% INPUT THE BODIES DETAILS HERE (Absolute Coordinate System)
 % First line defines body type.
-% Second line defines body's global position vector.
-% Third line defines length of teh body.
+% Second line defines length of the body
+% Third line defines body's global position vector initially.
 
-body1.type = ''2;
-body1.length = 1;
-body1.CMcord = [2; 2];
-
-body2.mass = 3;
-body2.length = 1;
-body2.CMcord = [4; 2];
-
-bodies = [body1, body2];
-
-
-
-% Define bodies and their initial positions
-type = 'slenderRod';        % Body type
-position = [0 L/2 pi/2]';   % Body initial position in global coordinates
-length = L;                 % Rod length
-body1 = struct('type',type,'position',position,'length',length);
-
-type = 'slenderRod';
-position = [L/2 L 0]';
+type = 'bar';
 length = L;
-body2 = struct('type',type,'position',position,'length',length);
+globalPosition = [0 L/2 pi/2]';
+body1 = struct('type',type,'length',length,'position',globalPosition);
 
-type = 'slenderRod';
-position = [L L/2 -pi/2]';
+type = 'bar';
 length = L;
-body3 = struct('type',type,'position',position,'length',length);
+globalPosition = [L/2 L 0]';
+body2 = struct('type',type,'length',length,'position',globalPosition);
 
-data.bodies = [body1,body2,body3];
+type = 'bar';
+length = L;
+globalPosition = [L L/2 -pi/2]';
+body3 = struct('type',type,'length',length,'position',globalPosition);
 
+InputParameters.bodies = [body1,body2,body3];
+
+%% INPUT THE CONSTRAINTS HERE (JOINTS)
+% First line indicates the joint type
+% Second line indicates the bodies involved in the joint (0 indicates ground)
+% Third line indicates the joint location following body reference
+% coordinate system
+
+jointType = 'revolute';
+jointBodies = [0 1];
+jointLocation = [[0 0]' [-L/2 0]'];
+joint1 = struct('type',jointType,'bodies',jointBodies,'position',jointLocation);
+
+jointType = 'revolute';
+jointBodies = [1 2];
+jointLocation = [[L/2 0]' [-L/2 0]'];
+joint2 = struct('type',jointType,'bodies',jointBodies,'position',jointLocation);
+
+jointType = 'revolute';
+jointBodies = [2 3];
+jointLocation = [[L/2 0]' [-L/2 0]'];
+joint3 = struct('type',jointType,'bodies',jointBodies,'position',jointLocation);
+
+jointType = 'revolute';
+jointBodies = [3 0];
+jointLocation = [[L/2 0]' [L 0]'];
+joint4 = struct('type',jointType,'bodies',jointBodies,'position',jointLocation);
+
+InputParameters.joints = [joint1,joint2,joint3,joint4];
 
 
 %% INPUT THE JOINT DETAILS HERE
